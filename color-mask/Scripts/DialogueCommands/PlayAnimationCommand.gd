628d533e384b88d4f545ed0290a8b0a12c8e1103
flakysalt <robinmhentschel123@googlemail.com>
2026-02-01 10:35:02 +0100

    merge camera and player

--- color-mask/Scripts/DialogueCommands/PlayAnimationCommand.gd
+++ color-mask/Scripts/DialogueCommands/PlayAnimationCommand.gd
@@ -4,8 +4,6 @@ class_name PlayAnimationCommand
 @export var anim_name : String
 @export var followupCommand: DialogueCommand
 
-
-
 func execute(caller : Node) -> void:
 	if caller.has_method("play_animation"):
 		caller.play_animation(anim_name)
