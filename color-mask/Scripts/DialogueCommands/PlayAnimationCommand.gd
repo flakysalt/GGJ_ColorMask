14a5d304bc26ab882635333672e91da4378eefa3
flakysalt <robinmhentschel123@googlemail.com>
2026-02-01 10:29:37 +0100

    add follow up command to anim command

--- color-mask/Scripts/DialogueCommands/PlayAnimationCommand.gd
+++ color-mask/Scripts/DialogueCommands/PlayAnimationCommand.gd
@@ -2,10 +2,17 @@ extends DialogueCommand
 class_name PlayAnimationCommand
 
 @export var anim_name : String
+@export var followupCommand: DialogueCommand
+
+
 
 func execute(caller : Node) -> void:
 	if caller.has_method("play_animation"):
 		caller.play_animation(anim_name)
 	else:
 		printerr("missing animation on ", caller.name)
+		
+	if followupCommand != null:
+		followupCommand.execute(caller)
+	pass
 	
