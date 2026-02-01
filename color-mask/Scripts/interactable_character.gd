2b6123df2fbf46a091df42489706dce666f2f1ec
flakysalt <robinmhentschel123@googlemail.com>
2026-02-01 15:16:01 +0100

    fix interactable

--- color-mask/Scripts/interactable_character.gd
+++ color-mask/Scripts/interactable_character.gd
@@ -12,12 +12,6 @@ func interact():
 		if GlobalState.hasState(entry.key):
 			continue
 		entry.command.execute(self)
-	
-	for state_key in dialogues:
-		if GlobalState.hasState(state_key):
-			continue
-		var command: DialogueCommand = dialogues[state_key]
-		command.execute(self)
 
 func play_animation(animation_name : String):
 	animationPlayer.play(animation_name)
