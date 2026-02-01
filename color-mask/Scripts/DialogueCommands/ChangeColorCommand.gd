7f92c935c44cd12f4d919bb4e2cc841cbbd7692b
flakysalt <robinmhentschel123@googlemail.com>
2026-02-01 15:58:24 +0100

    add follow ups

--- color-mask/Scripts/DialogueCommands/ChangeColorCommand.gd
+++ color-mask/Scripts/DialogueCommands/ChangeColorCommand.gd
@@ -4,7 +4,13 @@ class_name ChangeColorCommand
 @export var shaderPresetName : GlobalStateClass.MaskState
 @export var transitionTime : float = 3
 
+@export var followupCommand: DialogueCommand
+
+
 func execute(caller : Node) -> void:
 	var camera : ShaderCamera = caller.get_tree().get_first_node_in_group("camera")
 	camera.transition_to_preset(shaderPresetName,transitionTime)
 	GlobalState.set_mask_state(shaderPresetName)
+	
+	if followupCommand != null:
+		followupCommand.execute(caller)
