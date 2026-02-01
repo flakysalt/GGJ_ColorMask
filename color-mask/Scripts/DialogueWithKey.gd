5db105d07dc25dadb1c566ae5b38b569d8f50c04
flakysalt <robinmhentschel123@googlemail.com>
2026-02-01 14:42:57 +0100

    fix order of dialogue

---
+++ color-mask/Scripts/DialogueWithKey.gd
@@ -0,0 +1,6 @@
+extends Resource
+class_name DialogueWithKey
+
+
+@export var key : String
+@export var command : DialogueCommand
--- color-mask/Scripts/interactable_character.gd
+++ color-mask/Scripts/interactable_character.gd
@@ -1,11 +1,18 @@
 extends StaticBody2D
 
+
+#DONT USE THE DICT, doesnt save order
 @export var dialogues: Dictionary[String,DialogueCommand]
-@export var dialogue: DialogueCommand
+@export var dialogueList : Array[DialogueWithKey]
 
 @export var animationPlayer : AnimationPlayer
 
 func interact():
+	for entry in dialogueList:
+		if GlobalState.hasState(entry.key):
+			continue
+		entry.command.execute(self)
+	
 	for state_key in dialogues:
 		if GlobalState.hasState(state_key):
 			continue
